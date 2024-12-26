b14669190284cf04ececcc47c03a7935c41e8389
DanielKucherenko-kiu <kucherenko.daniel@kiu.edu.ge>
2024-12-26 18:41:14 +0400

    FIbonacci kotlin syntax
    this algorithms show the implementation of the kotlyn syntax for fibonacci numbers

--- .idea/Algoritms.kts
+++ .idea/Algoritms.kts
@@ -1,1 +1,18 @@
-fun fibonacciIterative(n: Int): Int {}
+n = input("Enter the value of n: ") // Get the position (n) of the Fibonacci number from the user
+a = 0
+b = 1
+i = 2  /
+
+if n == 0 {
+    fib = 0  // The 0th Fibonacci number is 0
+} else if n == 1 {
+    fib = 1  // The 1st Fibonacci number is 1
+} else {
+    while (i <= n) {
+        fib = a + b  // Compute the next Fibonacci number
+        a = b
+        b = fib
+        i = i + 1    // Move to the next position in the sequence
+    }
+}
+print(fib)  // Output the n-th Fibonacci number
