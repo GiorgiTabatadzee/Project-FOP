e696b960537bcc9aeac98631aff45aeb6a7bb27c
DanielKucherenko-kiu <kucherenko.daniel@kiu.edu.ge>
2024-12-26 19:00:12 +0400

    Multitable kotlin syntax
    This part is for the syntax of the multiplication table algorithm for kotlin
    
    Co-Authored-By: GiorgiTabatadzee <192440521+GiorgiTabatadzee@users.noreply.github.com>

---
+++ .idea/MultiplicationTable.kts
@@ -0,0 +1,10 @@
+n = input("Enter the number to generate its multiplication table: ") // Get the number for the table
+i = 1  // Start the multiplier at 1
+
+// Loop through numbers 1 to 10 to generate the multiplication table
+while (i <= 10) {
+    print(n * i)
+    i = i + 1
+}
+
+// The multiplication table is complete at this point
